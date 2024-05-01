cd7e9a79d41837f3032d7301353a44821533623b
Pravat Jung Basnet <pravatjungbasnet@gmail.com>
2024-05-01 15:19:50 +0545

    about us shop done

--- product/urls.py
+++ product/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .import views
 urlpatterns =[
     path('',views.index,name='index'),
+    path('about/',views.About,name='about'),
+    path('contact/',views.Contact,name='contact'),
+    path('shop/',views.Shop,name='shop'),
 ]
--- product/views.py
+++ product/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def About(request):
+    return render(request,'About.html')
+
+def Contact(request):
+    return render(request,'Contact.html')
+
+def Shop(request):
+    return render(request,'Shop.html')
